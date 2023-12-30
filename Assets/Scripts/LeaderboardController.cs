6fa17e14388eb4c553cb1a6238c6ee4b8a2a432a
Rectenko <98182873+graivyteg@users.noreply.github.com>
2023-12-30 12:48:44 +0300

    Bug Improvements

--- Assets/Scripts/LeaderboardController.cs
+++ Assets/Scripts/LeaderboardController.cs
@@ -21,14 +21,15 @@ public class LeaderboardController : MonoYandex
     {
         _record = YandexGame.savesData.Record;
     }
-
+    
     private void Update()
     {
         _timer -= Mathf.Min(_timer, Time.deltaTime);
-        if (_timer >= 0) return;
+        if (_timer > 0) return;
         
         if (_player.Wallet.Money > _record)
         {
+            Debug.Log("New Record!");
             YandexGame.NewLeaderboardScores(_leaderboardKey, _player.Wallet.Money);
             YandexGame.savesData.Record = _player.Wallet.Money;
             YandexGame.SaveProgress();
